49665abacd6f9bc908bda869b17fc4fce81cef69
artichoked1 <41409702+artichoked1@users.noreply.github.com>
2025-08-06 16:27:13 +0930

    cleanup formatting

--- main/lorawan_storage.hpp
+++ main/lorawan_storage.hpp
@@ -3,9 +3,9 @@
 #include <RadioLib.h>
 #include <cstdint>
 
-constexpr char LORAWAN_NAMESPACE[]     = "radiolib";
-constexpr char LORAWAN_KEY_NONCES[]    = "nonces";
-constexpr char LORAWAN_KEY_SESSION[]   = "session";
+constexpr char LORAWAN_NAMESPACE[] = "radiolib";
+constexpr char LORAWAN_KEY_NONCES[] = "nonces";
+constexpr char LORAWAN_KEY_SESSION[] = "session";
 
 namespace lorawan {
 int lwActivate(LoRaWANNode& node);
